812f00f1da8e5761a02f94ff1435fbf22a7c786d
欧阳芳芳 <1208389898@qq.com>
2018-05-21 10:13:55 +0800

    delete node_modules

--- myapp/app.js
+++ myapp/app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  console.log('oooooooos')
 }
 
 app.get('/', routes.index);
